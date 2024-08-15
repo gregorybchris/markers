7433bccfbe87f55ff6e19331d553924f582552d2
Chris Gregory <gregorybchris@gmail.com>
2024-08-14 22:25:50 -0700

    Add CLI tests

---
+++ tests/test_cli.py
@@ -0,0 +1,28 @@
+import pytest
+from click.testing import CliRunner
+from markers.cli import main
+
+
+@pytest.fixture(scope="session", name="cli_runner")
+def cli_runner_fixture() -> CliRunner:
+    return CliRunner()
+
+
+class TestCli:
+    def test_parse(self, cli_runner: CliRunner) -> None:
+        result = cli_runner.invoke(main, ["parse", "a and b"])
+        assert result.exit_code == 0
+        expected = (
+            "BinaryOp(\n"
+            "│   pos_info=PosInfo(line_no=1, char_no=3, length=3),\n"
+            "│   kind=<BinaryOpKind.AND: 'and'>,\n"
+            "│   left=Var(pos_info=PosInfo(line_no=1, char_no=1, length=1), name='a'),\n"
+            "│   right=Var(pos_info=PosInfo(line_no=1, char_no=7, length=1), name='b')\n"
+            ")\n"
+        )
+        assert result.output == expected
+
+    def test_eval(self, cli_runner: CliRunner) -> None:
+        result = cli_runner.invoke(main, ["eval", "a and b or d", "-t", "a", "-t", "b", "-f", "d"])
+        assert result.exit_code == 0
+        assert result.output == "True\n"
